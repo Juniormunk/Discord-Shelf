46500807b58ac117206ead4990d25150b978399f
Juniormunk <theforgelover@gmail.com>
2021-02-14 00:32:36 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,41 +1,18 @@
 #!/usr/bin/env bash
 #bash <(curl -L https://github.com/resin-io/resin-wifi-connect/raw/master/scripts/raspbian-install.sh) -- -y
 
+cd /home/pi/
 
-
-printf "#!/usr/bin/env bash
-
-# export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-# ^^^ this can cause barfing and isn't needed
-
-# Choose a condition for running WiFi Connect according to your use case:
-
-# 1. Is there a default gateway?
-# ip route | grep default
-
-# 2. Is there Internet connectivity?
-# nmcli -t g | grep full
-
-# 3. Is there Internet connectivity via a google ping?
-wget --spider http://google.com 2>&1
-
-# 4. Is there an active WiFi connection?
-#iwgetid -r
-
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    wifi-connect --portal-ssid Discord_Shelf
-
-fi"> /home/pi/WIFIScript.sh
+wget -O install.sh https://raw.githubusercontent.com/Juniormunk/Discord-Shelf/main/WIFIScript.py
 
 chmod +x /home/pi/WIFIScript.sh
 
-lines="*/5 * * * * ./home/pi/WIFIScript.sh\n @reboot ./home/pi/WIFIScript.sh" 
+lines="*/5 * * * * ./home/pi/WIFIScript.sh\n"
 ( crontab -u pi -l; echo "$lines" ) | crontab -u pi -
 
-cd /home/pi/
+lines="@reboot ./home/pi/WIFIScript.sh" 
+( crontab -u pi -l; echo "$lines" ) | crontab -u pi -
+
 wget -O Shelf.py https://raw.githubusercontent.com/Juniormunk/Discord-Shelf/main/Shelf.py
 
 
@@ -55,5 +32,3 @@ ExecStart=python3 /home/pi/Shelf.py
 WantedBy=multi-user.target" > /etc/systemd/system/discordshelf.service
 
 systemctl enable discordshelf
-
-reboot

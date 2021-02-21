6d1054912616e7c255fa310266b078ae853b7680
Juniormunk <theforgelover@gmail.com>
2021-02-21 15:44:47 -0500

    actually working...

--- Shelf.py
+++ Shelf.py
@@ -30,6 +30,7 @@ if not os.path.exists('/boot/shelf_config.json'):
     with open('/boot/shelf_config.json', 'w') as file:
         file.write('''{
   "Discord Bot Token": "",
+  "Fade Speed": 0.01,
   "Slots": [[1,2,3],[4,5,6],[7,8,9],[10,11,12]],
   "Guild ID": 0,
   "Users": [
@@ -70,7 +71,7 @@ if not os.path.exists('/boot/shelf_config.json'):
 }''')
         file.close()
         
-logging.basicConfig(filename='/boot/latest.log',level=logging.DEBUG)
+logging.basicConfig(filename='/boot/latest.log',level=logging.ERROR)
 
         
 def openConfig():
@@ -80,7 +81,8 @@ def openConfig():
         with open('/boot/shelf_config.json', 'r') as infile:
             config = json.loads(infile.read())
             infile.close()
-    except json.decoder.JSONDecodeError:
+    except json.decoder.JSONDecodeError as e:
+        logging.error(e)
         logging.error("Could not parsing json file!")
         lightStatus = LightStatus.JSONError
         x.start()
@@ -89,6 +91,8 @@ def openConfig():
             
 openConfig()
 
+
+lastBotToken = ""
 def getConfigBotToken():
     return config["Discord Bot Token"]
 def getConfigGuildID():
@@ -126,104 +130,149 @@ def saveConfig():
 
 @app.route('/', methods = ['POST', 'GET'])
 def homePage():
-    global config
-        
-    output = ""
-    output += "<html>"
-    output += "<head>"
-    output += '''<link rel="icon" type="image/svg+xml" href="http://discord.com/assets/41484d92c876f76b20c7f746221e8151.svg">'''
-    output += "<title>Discord Shelf</title>"
-    output += '''<style type="text/css">
-                        #container { 
-                        width:70%;
-                        height:80%;
-                        padding:5px 7px 5px 5px; 
-                        }
-                        #txtarea {
-                        font-size:16;
-                        resize:none;
-                        display:block;
-                        background: #23272A;
-                        width:99%;
-                        height:99%;
-                        padding:0.5%;
-                        margin:0;
-                        overflow-y: scroll;
-                        overflow-x: scroll;
-                            color: #FFFFFF;
-                        }
-                        .button {
-                          background-color: #4CAF50; /* Green */
-                          border: none;
-                          color: white;
-                          padding: 15px 32px;
-                          text-align: center;
-                          text-decoration: none;
-                          display: inline-block;
-                          font-size: 16px;
-                          margin: 4px 2px;
-                          cursor: pointer;
-                        }
-                        .button4 {background-color: #e7e7e7; color: black;} /* Gray */ 
-                        </style>'''
-    output += "</head>"
-    output += "<center>"
-    output += '''<body style="background-color:#23272A;">'''
-    output += '''<h1 style="color:#7289DA;">Welcome to the discord shelf configuration editor. Below you will find a json file that will allow you to configure your shelf.</h1>'''
-    output += '''<div id="container">'''
-    if request.method == 'GET':
-        with open('/boot/shelf_config.json', 'r') as infile:
-                file = infile.read()
-                infile.close()
-        output += '''<form method='POST' enctype='multipart/form-data' action='/'>
-                    <textarea id="txtarea" name="json">'''+file+'''</textarea>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <input class="button button4" type="submit" value="Apply">
-                    </form>'''
-    if request.method == 'POST':
-        try:
-            config = json.loads(request.form["json"])
-            saveConfig()
-            loadConfig()
-            updateFriendStatus()
+    try:
+        global config
+            
+        output = ""
+        output += "<html>"
+        output += "<head>"
+        output += '''<link rel="icon" type="image/svg+xml" href="http://discord.com/assets/41484d92c876f76b20c7f746221e8151.svg">'''
+        output += "<title>Discord Shelf</title>"
+        output += '''<style type="text/css">
+                            #container { 
+                            width:70%;
+                            height:80%;
+                            padding:5px 7px 5px 5px; 
+                            }
+                            #txtarea {
+                            font-size:16;
+                            resize:none;
+                            display:block;
+                            background: #23272A;
+                            width:99%;
+                            height:99%;
+                            padding:0.5%;
+                            margin:0;
+                            overflow-y: scroll;
+                            overflow-x: scroll;
+                                color: #FFFFFF;
+                            }
+                            .button {
+                              background-color: #4CAF50; /* Green */
+                              border: none;
+                              color: white;
+                              padding: 15px 32px;
+                              text-align: center;
+                              text-decoration: none;
+                              display: inline-block;
+                              font-size: 16px;
+                              margin: 4px 2px;
+                              cursor: pointer;
+                            }
+                            .button4 {background-color: #e7e7e7; color: black;} /* Gray */ 
+                            </style>'''
+        output += "</head>"
+        output += "<center>"
+        output += '''<body style="background-color:#23272A;">'''
+        output += '''<h1 style="color:#7289DA;">Welcome to the discord shelf configuration editor. Below you will find a json file that will allow you to configure your shelf.</h1>'''
+        output += '''<div id="container">'''
+        if request.method == 'GET':
             with open('/boot/shelf_config.json', 'r') as infile:
-                file = infile.read()
-                infile.close()
+                    file = infile.read()
+                    infile.close()
             output += '''<form method='POST' enctype='multipart/form-data' action='/'>
-                    <textarea id="txtarea" name="json">'''+file+'''</textarea>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <input class="button button4" type="submit" value="Apply">
-                    </form>'''
-        except:
-            with open('/boot/shelf_config.json', 'r') as infile:
-                file = infile.read()
-                infile.close()
-            config = json.loads(file)
-            saveConfig()
-            loadConfig()
-            updateFriendStatus()
-            output+='''<h1 style="color:RED;">Error parsing json file</h1>'''
-            output += '''<form method='POST' enctype='multipart/form-data' action='/'>
-                    <textarea id="txtarea" name="json">'''+request.form["json"]+'''</textarea>
-                    <br>
-                    <br>
-                    <br>
-                    <br>
-                    <input class="button button4" type="submit" value="Apply">
-                    </form>'''  
-   
-    output += '''</div>'''
-    output += "</center>"
-    output += "</body>"
-    output += "</html>"
-    return output
-
+                        <textarea id="txtarea" name="json">'''+file+'''</textarea>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <input class="button button4" type="submit" value="Apply">
+                        </form>'''
+        if request.method == 'POST':
+            try:
+                config = json.loads(request.form["json"])
+                saveConfig()
+                with open('/boot/shelf_config.json', 'r') as infile:
+                    file = infile.read()
+                    infile.close()
+                if lastBotToken != getConfigBotToken():
+                   output+='''<h1 style="color:RED;">You must restart to apply some settings changed.</h1>'''
+                if lastBotToken == getConfigBotToken():
+                    loadConfig()
+                    updateFriendStatus()
+                
+                output += '''<form method='POST' enctype='multipart/form-data' action='/'>
+                        <textarea id="txtarea" name="json">'''+file+'''</textarea>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <input class="button button4" type="submit" value="Apply">
+                        </form>'''
+            except Exception as e:
+                logging.error(e)
+                with open('/boot/shelf_config.json', 'r') as infile:
+                    file = infile.read()
+                    infile.close()
+                config = json.loads(file)
+                saveConfig()
+                loadConfig()
+                updateFriendStatus()
+                output+='''<h1 style="color:RED;">Error parsing json file</h1>'''
+                output += '''<form method='POST' enctype='multipart/form-data' action='/'>
+                        <textarea id="txtarea" name="json">'''+request.form["json"]+'''</textarea>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <input class="button button4" type="submit" value="Apply">
+                        </form>'''  
+       
+        output += '''</div>'''
+        output += "</center>"
+        output += "</body>"
+        output += "</html>"
+        return output
+    except Exception as e:
+        logging.error(e)
+        output = ""
+        output += "<html>"
+        output += "<head>"
+        output += '''<link rel="icon" type="image/svg+xml" href="http://discord.com/assets/41484d92c876f76b20c7f746221e8151.svg">'''
+        output += "<title>Discord Shelf</title>"
+        output += '''<style>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+}
+</style>'''
+        output += "</head>"
+        output += "<center>"
+        output += '''<body style="background-color:#23272A;">'''
+        output += '''<div id="container">'''
+        output+='''<h1 style="color:RED;">An error has occured please restart. If this error continues please contact the developer.</h1>'''
+        '''<form action='/logs'>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <input class="button button4" type="logs" value="View Logs">
+                        </form>'''
+        output += '''</div>'''
+        output += "</center>"
+        output += "</body>"
+        output += "</html>"
+        return output
+@app.route('/log')
+def logPage():
+    log="Error finding log file... please contact developer"
+    with open('/boot/latest.log', 'r') as infile:
+            log = infile.read()
+            infile.close()
+    return log
+    
 def serverThread():
     app.run(host='0.0.0.0', port=80)
 
@@ -283,7 +332,8 @@ def lightThread():
     direction = -1
     try:
         step = getConfigFadeSpeed()
-    except:
+    except Exception as e:
+        logging.error(e)
         step = .08
     while True:
         try:
@@ -444,7 +494,7 @@ while(checkInternetUrllib()==False):
     time.sleep(1)
     lightStatus = LightStatus.WIFIError
 
-
+lastBotToken = getConfigBotToken()
 client.run(getConfigBotToken())
 
 
